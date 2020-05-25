e2ef8fd12d38c1da02a1fc0050af68e77865e4d1
vincent <>
2020-05-25 18:55:14 +0800

    up

---
+++ bin/train.sh
@@ -0,0 +1,13 @@
+# 训练数据
+Date=$(date +%Y%m%d%H%M)
+
+log_dir="logs"
+
+if [ ! -d "$log_dir" ]; then
+        mkdir $log_dir
+fi
+
+nohup \
+python -m mask_train \
+>> ./logs/console_$Date.log 2>&1 &
+echo "启动完毕,在logs下查看日志！"
