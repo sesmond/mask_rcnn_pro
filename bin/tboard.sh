0595ada4874b73ca8710b2307173b6c73ff3d918
vincent <>
2020-06-09 17:57:36 +0800

    git push

---
+++ bin/tboard.sh
@@ -0,0 +1,18 @@
+if [ "$1" = "" ] || [ "$1" = "help" ]; then
+   echo "Usage: tboard.sh <port> <#gpu_id>"
+   echo "       tboard.sh stop"
+   exit
+fi
+set -x
+if [ "$1" = "stop" ]; then
+   ps aux|grep tensorboard|grep mask_rcnn|awk '{print $2}'| xargs kill
+   echo "[mask_rcnn]TensorBoard退出..."
+   exit
+fi
+
+gpu=3
+
+log_path=logs/
+echo "启动 $1 的tensorboard日志...."
+echo "端口：$port，日志路径：$log_path"
+CUDA_VISIBLE_DEVICES=$gpu nohup /root/py3/bin/tensorboard --port=$port --logdir=$log_path >./tboard.log 2>&1 &
