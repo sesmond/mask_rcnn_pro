e814241761f16dd9c6bfb95a964b32a92508b9d5
MaYanMei <yanmeima3@creditease.cn>
2020-07-02 17:49:28 +0800

    pre idcard

--- mym_test.py
+++ mym_test.py
@@ -1,70 +1,105 @@
 #!/usr/bin/env python
 # _*_ coding:utf-8 _*_
+# ============================================
+# @Time     : 2020/05/18 14:42
+# @Author   : WanDaoYi
+# @FileName : mask_test.py
+# ============================================
 
-import cv2
+from datetime import datetime
 import os
-import datetime
-from mask_test import MaskTest
+import colorsys
+import cv2
+import skimage.io
+import numpy as np
+from matplotlib import patches
+import matplotlib.pyplot as plt
 from m_rcnn.mask_rcnn import MaskRCNN
+from matplotlib.patches import Polygon
+from skimage.measure import find_contours
+from config import cfg
+os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
 
 
+class MaskTest(object):
 
-def cut_rectangle(box,image):
-    '''
-    抠出外接矩形
-    :return:
-    '''
-    ymin = box[0][0]
-    xmin = box[0][1]
-    ymax = box[0][2]
-    xmax = box[0][3]
-    w = xmax - xmin
-    h = ymax - ymin
-    cut_img = image[ymin:ymin+h,xmin:xmin+w]
+    def __init__(self):
 
-    return cut_img
+        # 获取 类别 list
+        #TODO !! 分类数据
+        self.class_names_path = cfg.COMMON.COCO_CLASS_NAMES_PATH
+        # self.class_names_path = cfg.COMMON.OUR_CLASS_NAMES_PATH
+        self.class_names_list = self.read_class_name()
 
+        # 测试图像的输入 和 输出 路径
+        self.test_image_file_path = cfg.TEST.TEST_IMAGE_FILE_PATH
+        self.output_image_path = cfg.TEST.OUTPUT_IMAGE_PATH
 
+        # 加载网络模型
+        self.mask_model = MaskRCNN(train_flag=False)
+        # 加载权重模型
+        self.mask_model.load_weights(cfg.TEST.COCO_MODEL_PATH, by_name=True)
 
-def mask_result(test_image_file_path):
-    mask_test = MaskTest()
-    mask_model = MaskRCNN(train_flag=False)
+        pass
 
-    test_image_name_list = mask_test.get_images(test_image_file_path)
+    def read_class_name(self):
+        with open(self.class_names_path, "r") as file:
+            class_names_info = file.readlines()
+            class_names_list = [class_names.strip() for class_names in class_names_info]
 
-    for test_image_path in test_image_name_list:
-        test_image_name = os.path.basename(test_image_path)
+            return class_names_list
+        pass
 
-        # 读取图像
-        image_info = cv2.imread(test_image_path)
-        print("read img:", test_image_path)
+    def get_images(self,data_path):
+        '''
+        find image files in test data path
+        :return: list of files found
+        '''
+        files = []
+        exts = ['jpg', 'png', 'jpeg', 'JPG']
+        for parent, dirnames, filenames in os.walk(data_path):
+            for filename in filenames:
+                for ext in exts:
+                    if filename.endswith(ext):
+                        files.append(os.path.join(parent, filename))
+                        break
+        return files
 
-        # Run detection
-        results_info_list = mask_model.detect([image_info])
-        print("results: {}".format(results_info_list))
+    def do_test(self, show_image_flag=False):
+        """
+            batch predict
+        :param show_image_flag: show images or not
+        :return:
+        """
+        test_image_name_list = self.get_images(self.test_image_file_path)
 
-    return results_info_list, image_info, test_image_name
+        for test_image_path in test_image_name_list:
+            test_image_name = os.path.basename(test_image_path)
+            # test_image_path = os.path.join(self.test_image_file_path, test_image_name)
+            # 读取图像
+            # image_info = skimage.io.imread(test_image_path)
+            image_info = cv2.imread(test_image_path)
+            print("read img:",test_image_path)
+            # Run detection
+            results_info_list = self.mask_model.detect([image_info])
+            print("results: {}".format(results_info_list))
 
-
-
-
-def result_analysis(test_image_file_path,cut_img_path):
-    results,image,test_image_name = mask_result(test_image_file_path)
-
-    box = results[0]['rois']
-    mask = results[0]['masks']
-
-    cut_img = cut_rectangle(box, image)
-    cv2.imwrite(os.path.join(cut_img_path + test_image_name),cut_img)
-
-    return cut_img
-
+        pass
 
 
 
 
 if __name__ == "__main__":
-    test_image_file_path = "data/idcard/test/input/"
-    cut_img_path = "data/idcard/test/cut/"
+    # 代码开始时间
+    start_time = datetime.now()
+    print("开始时间: {}".format(start_time))
 
-    cut_img = result_analysis(test_image_file_path,cut_img_path)
+    demo = MaskTest()
+    # print(demo.class_names_list)
+    demo.do_test()
+
+    # 代码结束时间
+    end_time = datetime.now()
+    print("结束时间: {}, 测试模型耗时: {}".format(end_time, end_time - start_time))
+
+
